28d81c3f13f1310c4cbce57b11c131d1578e08ee
Ben Hopkins <bjhops17@gmail.com>
2020-12-23 16:44:01 -0800

    linked in to drop down and added search api though not functional yet.

--- script.js
+++ script.js
@@ -2,43 +2,26 @@
 $(".dropdown-trigger").dropdown();
 // THIS IS A AN API WITH THE LIST OF GENRES AND THEIR CODES. WE NEED THE CODES TO BE PLACED INTO THE BELOW FUNCTION WHEN THE RESPECTIVE BUTTON IS CLICKED
 
+//THIS EVENTLISTENER PULLS A RANDOM MOVIE OUT OF A LIST BASED ON GENRE. THE LAST PART OF THE BELOW URL WHERE IT SAYS GENRE IS WHERE THE CODE NEEDS TO GO. Click test button to see console.log. Okay to delte 
 
-// function listOfGenres(){
-// var genreList = "https://api.themoviedb.org/3/genre/movie/list?api_key=dc7d76692b192b772ecce4d938dfa475&language=en-US" 
-// $.ajax({
-//     url: genreList,
-//     method: "GET"
-// }).then(function(genres){
-//     for (var i = 0; i < genres.genres.length; i++){
-//     console.log(genres.genres[i].name)
-//     $("<button>").text(genres.genres[i].name)
-//     $(".container").append($("<button>").attr("data-genre", genres.genres[i].id).text(genres.genres[i].name))
-//     }
-
-// })
-// }
-// listOfGenres()
-// THIS EVENTLISTENER PULLS A RANDOM MOVIE OUT OF A LIST BASED ON GENRE. THE LAST PART OF THE BELOW URL WHERE IT SAYS GENRE IS WHERE THE CODE NEEDS TO GO. Click test button to see console.log. Okay to delte Test button. Only their for testing.
-$(".test").on("click", function(event){
-
-  var genre = $(event.target).attr("data-set")
+$(".dropdown-content").on("click", function(event){
+  
+  var genre = $(event.target).attr("data-id")
+  console.log(genre)
   var movieList = "https://api.themoviedb.org/3/discover/movie?api_key=dc7d76692b192b772ecce4d938dfa475&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1&with_genres=" + genre  
   
   $.ajax({
       url: movieList,
       method: "GET"
   }).then(function(movies){
+      console.log(movies)
    var randomMovie = Math.floor(Math.random() * 19)
    var movieId = movies.results[randomMovie].id
    
    ///calls streem function with movieId as variable
   streem(movieId)
   })
-  
-  
-  })
-  
-  
+})
   
   // Function For Streeming Data//Insert movieId where x is.
   function streem(x){
@@ -54,28 +37,35 @@ $(".test").on("click", function(event){
       var voterRate = streeming.vote_average // For voteer rating
   
   
-      $(".movie").text("Title: " + title)
-      $(".movie").append($("<img>").attr("src", "https://image.tmdb.org/t/p/w500" + poster))
-      $("img").attr("height", "120vw")
+      $(".movie-display").text("Title: " + title)
+      $(".movie-display").append($("<img>").attr("src", "https://image.tmdb.org/t/p/w500" + poster))
+      $("img").attr("height", "320vw")
       
-      $(".movie").append(synops)
-      $(".movie").append("Stars: " + voterRate)
+      $(".movie-display").append(synops)
+      $(".movie-display").append("Stars: " + voterRate)
      
-      function perscription (){
       var subscription = streeming["watch/providers"].results.US.flatrate
-      for (var i = 0; i < subscription.length; i++)
-      $("<link>").text(subscription[i].provider_name)
-      $(".movie").append($("link"))
-      }
-      var rent = streeming["watch/providers"].results.US.rent
+      for (var i = 0; i < subscription.length; i++){
+      $(".movie-display").append($("<a>").text("streem " + subscription[i].provider_name))
       
-      perscription()
+    }
+      var rent = streeming["watch/providers"].results.US.rent
+      for (var i = 0; i < rent.length; i++){
+      $(".movie-display").append($("<a>").text("Available for rent at: " + rent[i].provider_name))
+      }
       
   })
   }
   
   
   
+  function SerchByName(){
   
-  
-  
+  var searchName = "https://api.themoviedb.org/3/search/movie?api_key=dc7d76692b192b772ecce4d938dfa475&query=Black+Hawk+Down"
+  $.ajax({
+      url: searchName,
+      method: "GET"
+  }).then(function(movieName){
+      console.log(movieName)
+  })
+  }
